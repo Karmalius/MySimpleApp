9885a09d0b5a8b77f75d434a8ec1ec8615b628cd
Karmalius <55229180+Misumuru@users.noreply.github.com>
2021-04-28 10:24:02 +0300

    Added search icon and list

--- myApp/src/pages/Bookshelf.tsx
+++ myApp/src/pages/Bookshelf.tsx
@@ -1,19 +1,61 @@
-import { IonContent,IonPage, IonToolbar, IonThumbnail, IonHeader, IonIcon, IonList, IonItem,  } from '@ionic/react';
-import './Bookshelf.css';
-import { menuOutline } from "ionicons/icons"
+import {
+  IonContent,
+  IonPage,
+  IonToolbar,
+  IonHeader,
+  IonIcon,
+  IonList,
+  IonItem,
+  IonLabel,
+} from "@ionic/react";
+import "./Bookshelf.css";
+import { menuOutline } from "ionicons/icons";
+import { searchOutline } from "ionicons/icons";
 
 const Bookshelf: React.FC = () => {
   return (
     <IonPage>
-      <IonToolbar className="toolbar">
-      </IonToolbar>
+      <IonToolbar className="toolbar"></IonToolbar>
       <IonContent fullscreen>
         <IonHeader className="header">
-            <IonIcon icon={menuOutline}></IonIcon>Bookshelf</IonHeader>
-        <IonThumbnail>
+          <IonIcon icon={menuOutline} className="menu"></IonIcon>Bookshelf
+          <IonIcon icon={searchOutline} className="search"></IonIcon>
+        </IonHeader>
+
         <IonList>
+          <IonItem>
+            <IonLabel>
+              <h2>Book Example</h2>
+              <p>Sed ut perspiciatis, unde omnis iste</p>
+              <p> natus error sit voluptatem </p>
+              <p>accusantium dolorem..</p>
+            </IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>
+              <h2>Book Title</h2>
+              <p>Sed ut perspiciatis, unde omnis iste</p>
+              <p> natus error sit voluptatem </p>
+              <p>accusantium dolorem..</p>
+            </IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>
+              <h2>Book Title</h2>
+              <p>Sed ut perspiciatis, unde omnis iste</p>
+              <p> natus error sit voluptatem </p>
+              <p>accusantium dolorem..</p>
+            </IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>
+              <h2>Book Title</h2>
+              <p>Sed ut perspiciatis, unde omnis iste</p>
+              <p> natus error sit voluptatem </p>
+              <p>accusantium dolorem..</p>
+            </IonLabel>
+          </IonItem>
         </IonList>
-    </IonThumbnail>
       </IonContent>
     </IonPage>
   );
