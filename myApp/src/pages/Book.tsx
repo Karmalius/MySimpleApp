99bf1549e9e5f9c49cf6e57955ac97186d5d6454
Karmalius <55229180+Misumuru@users.noreply.github.com>
2021-04-28 10:54:26 +0300

    Added card component with image and content

--- myApp/src/pages/Book.tsx
+++ myApp/src/pages/Book.tsx
@@ -4,8 +4,12 @@ import {
     IonToolbar,
     IonHeader,
     IonIcon,
+    IonCard,
+    IonCardContent,
+    IonImg,
+
   } from "@ionic/react";
-  import "./Bookshelf.css";
+  import "./Book.css";
   import { arrowBack } from "ionicons/icons";
 
   
@@ -17,6 +21,14 @@ import {
           <IonHeader className="header">
           <IonIcon icon={arrowBack}></IonIcon>Book Example
           </IonHeader>
+          <IonCard>
+          <IonCardContent>
+          <IonImg src={"assets/img/gothic.jpg"} />
+          Sed ut perspiciatis, unde omnis iste natus error sit 
+          voluptatem accusant doloremque laudantium, totam rem 
+          aperiam eaque ipsa, quae ab illo inventore veritatis.
+      </IonCardContent>
+          </IonCard>
         </IonContent>
       </IonPage>
     );
