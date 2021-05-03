0f9f08a762b8c2e7ffff1fa7d803ec32fc1a824e
Karmalius <55229180+Misumuru@users.noreply.github.com>
2021-05-03 09:17:13 +0300

    Tried to add some routing

--- myApp/src/pages/Welcome.tsx
+++ myApp/src/pages/Welcome.tsx
@@ -1,7 +1,28 @@
-import { IonContent,IonPage, IonToolbar, IonButton, IonThumbnail, IonImg  } from '@ionic/react';
+import { IonContent,IonPage, IonToolbar, IonButton, IonThumbnail, IonImg, IonRouterOutlet,  } from '@ionic/react';
+import { Redirect, Route } from 'react-router-dom';
 import './Welcome.css';
+import { IonReactRouter } from '@ionic/react-router';
+import LogIn from './LogIn';
+
+const directLogIn = () => {
+  return (
+  <IonReactRouter>
+    <IonRouterOutlet>
+      <Route exact path="/LogIn">
+        <LogIn />
+      </Route>
+      <Route exact path="/">
+        <Redirect to="/LogIn" />
+      </Route>
+    </IonRouterOutlet>
+  </IonReactRouter>
+  )
+}
 
 const Welcome: React.FC = () => {
+
+  
+
   return (
     <IonPage>
       <IonToolbar className="toolbar">
@@ -11,7 +32,7 @@ const Welcome: React.FC = () => {
         <IonThumbnail>
         <IonImg src={"assets/img/logo.jpg"} />
     </IonThumbnail>
-      <IonButton expand="full" className="login">Log in</IonButton>
+      <IonButton expand="full" className="login" onClick={directLogIn}>Log in</IonButton>
       <IonButton expand="full" className="signup">Sign up</IonButton>
       </IonContent>
     </IonPage>
