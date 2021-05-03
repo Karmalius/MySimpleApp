58bac1ef07d847e500521dfc301f47005eebcfbb
Karmalius <55229180+Misumuru@users.noreply.github.com>
2021-05-03 10:40:37 +0300

    Deleted onClickHandler and added className to thumbnail

--- myApp/src/pages/Welcome.tsx
+++ myApp/src/pages/Welcome.tsx
@@ -1,23 +1,19 @@
+
 import { IonContent,IonPage, IonToolbar, IonButton, IonThumbnail, IonImg,  } from '@ionic/react';
 import './Welcome.css';
 
-
-
 const Welcome: React.FC = () => {
 
-  const onClickHandler = () => {};
 
   return (
     <IonPage>
       <IonToolbar className="toolbar">
       </IonToolbar>
       <IonContent fullscreen>
-
-        <IonThumbnail>
+        <IonThumbnail className="pic">
         <IonImg src={"assets/img/logo.jpg"} />
     </IonThumbnail>
-      <IonButton expand="full" className="login" routerLink="/LogIn"
-            onClick={() => onClickHandler()}>Log in</IonButton>
+      <IonButton expand="full" className="login" routerLink="/LogIn">Log in</IonButton>
       <IonButton expand="full" className="signup">Sign up</IonButton>
       </IonContent>
     </IonPage>
