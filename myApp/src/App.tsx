f59c8a9b1b5c3d9de58c021126aa46d62465e541
Karmalius <55229180+Misumuru@users.noreply.github.com>
2021-05-04 09:50:02 +0300

    Added splitpane and imported menu page

--- myApp/src/App.tsx
+++ myApp/src/App.tsx
@@ -1,54 +1,41 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Welcome from './pages/Welcome';
-import LogIn from './pages/LogIn';
-import SignUp from './pages/SignUp';
-import Bookshelf from './pages/Bookshelf';
-import Book from './pages/Book';
+import { Redirect, Route } from "react-router-dom";
+import { IonApp, IonRouterOutlet, IonSplitPane } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import Welcome from "./pages/Welcome";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
+import Bookshelf from "./pages/Bookshelf";
+import Book from "./pages/Book";
+import Menu from "./components/Menu";
 
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
-
-/* Theme variables */
-import './theme/variables.css';
+import "@ionic/react/css/core.css";
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/welcome">
-          <Welcome />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/welcome" />
-        </Route>
-        <Route exact path="/login">
-        <LogIn />
-        </Route>
-        <Route exact path="/signup">
-        <SignUp />
-        </Route>
-        <Route exact path="/bookshelf">
-        <Bookshelf />
-        </Route>
-        <Route exact path="/book">
-        <Book />
-        </Route>
-      </IonRouterOutlet>
+      <IonSplitPane contentId="main">
+        <Menu />
+        <IonRouterOutlet id="main">
+          <Route exact path="/welcome">
+            <Welcome />
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/welcome" />
+          </Route>
+          <Route exact path="/login">
+            <LogIn />
+          </Route>
+          <Route exact path="/signup">
+            <SignUp />
+          </Route>
+          <Route exact path="/bookshelf">
+            <Bookshelf />
+          </Route>
+          <Route exact path="/book">
+            <Book />
+          </Route>
+        </IonRouterOutlet>
+      </IonSplitPane>
     </IonReactRouter>
   </IonApp>
 );
